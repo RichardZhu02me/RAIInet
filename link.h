5389e933e1ef72af55a8e77ed5ad979b10c86c84
Calista Kurniawan <78513510+calikoOoOo@users.noreply.github.com>
2024-11-23 16:20:55 -0500

    Update link.h

--- link.h
+++ link.h
@@ -5,10 +5,30 @@
 
 class Link{
     int travelDistance;
+    int row;
+    int col;
+    char id;
+    int type;
+    
     string linkType;
     bool hidden;
     Player* owner;
     public:
+        Link(char id, int type, int strength);
+        ~Link();
+        //getters:
+        int getRow() const;
+        int getCol() const;
+        char getId() const;
+        int getType() const;
+        
+        void up();
+        void down();
+        void left();
+        void right();
+
+        void isHidden();
+        void isDownloaded();
         //increase the travel distance by 1
         void boost();
         //set the link to the opposite linkType
