f7fe87be47fcf38b621dcebcfc66020d7026b9be
Calista Kurniawan <78513510+calikoOoOo@users.noreply.github.com>
2024-11-23 11:38:09 -0500

    Update player.h

--- player.h
+++ player.h
@@ -3,10 +3,15 @@
 
 #include "link.h"
 #include <vector>
+#include <memory>
+
 
 class Player{
     std::vector<Link*> links;
 public:
+    // currently for basic player
+    Player(std::string abilityOrder, std::string linkOrder, int number);
+    ~Player();
     void download(Link* link);
 };
 #endif
