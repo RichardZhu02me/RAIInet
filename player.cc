e7500f1975d03281d33b09603ec19ad76d972183
Calista Kurniawan <78513510+calikoOoOo@users.noreply.github.com>
2024-11-23 22:35:27 -0500

    Update player.cc

--- player.cc
+++ player.cc
@@ -1,1 +1,21 @@
+ #include "player.h"
+ #include <iostream>
+ using namespace std;
 
+Player::Player(string abilityOrder, string linkOrder, int number) :
+playerNumber{number} {
+    setAbilities(abilityOrder);
+    setLinks(linkOrder);
+}
+
+Player::~Player(){
+    vector <Cell*> fwCells;
+    for(unsigned int i=0; i< fwCells.size(); ++i){
+        fwCells[i]->isFireWall = false;
+        fwCells[i]->fireWallOwner = 0;
+    }
+    for (int i = 0; i < 2; ++i) {
+        SSCells[i]->isServerPort = false;
+        SSCells[i]->whoseServerPort = 0;
+    }
+}
