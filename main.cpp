815da1773b9a84b7349017e675fceb13950109eb
Bzxchen <bzxchen@gmail.com>
2024-11-26 21:39:33 -0500

    finished main

--- main.cpp
+++ main.cpp
@@ -1,123 +1,136 @@
 #include <vector>
 #include <iostream>
-#include "grid.h"
+#include <string>
+#include <algorithm>
+#include <random>
+#include "game.h"
 #include "player.h"
 
 using namespace std;
 
-void PlayerSetup(unique_ptr<Player>& pl, char c) {
-
-    while (pl->getNumOfAbLeft() != 5) { //selects and sets up abilites of the player
-        string ability;
-
-        cout << "P" << pl->getPlayerNum() << ": You have " << pl->getNumOfAbLeft() << " abilities set!" << endl
-            << "Please input the next ability you would like you set!" << endl;
-        cin >> ability;
-        if (ability == "download") {
-            if (pl->getNumDownload() < 2) {
-                pl->setAbility(ability);
-            } else {
-                cout << "You already have 2 " << ability << "s" << endl;
-            }
-        } else if (ability == "firewall") {
-            if (pl->getNumFirewall() < 2) {
-                pl->setAbility(ability);
-            } else {
-                cout << "You already have 2 " << ability << "s" << endl;
-            }
-        } else if (ability == "linkboost") {
-            if (pl->getNumLinkboost() < 2) {
-                pl->setAbility(ability);
-            } else {
-                cout << "You already have 2 " << ability << "s" << endl;
-            }
-        } else if (ability == "polarize") {
-            if (pl->getNumPolarize() < 2) {
-                pl->setAbility(ability);
-            } else {
-                cout << "You already have 2 " << ability << "s" << endl;
-            }
-        } else if (ability == "scan") {
-            if (pl->getNumScan() < 2) {
-                pl->setAbility(ability);
-            } else {
-                cout << "You already have 2 " << ability << "s" << endl;
-            }
-        }
-    }
-
-    char currLink = c;
-    int type;
-    int strength;
-    for (int i = 0; i < 8; i++) {
-        bool found = false;
-        for (auto link : pl->links) {
-            if (link->getType() == type && link->getStrength() == strength) {
-                found = true;
-                break;
-            }
-        }
-        if(!found) {
-            pl->setLink(currLink, type, strength, pl);
-            c++;
-        } else {
-            cout << "Invalid input, please try again!" << endl;
-            i--;
-        }
+void abilitySetup(unique_ptr<Game>& g, int playerNum, string abilities) {
+    unique_ptr<Player> pl = g->players[playerNum];
+    for (int i = 0; i < 5; i++) {
+        pl->setAbility(abilities[i]);
     }
 }
 
-void GridSetup(unique_ptr<Grid>& g, unique_ptr<Player>& p1, unique_ptr<Player>& p2) {
+void linkSetup(unique_ptr<Game>& g, int playerNum, string linkFile) {
+    unique_ptr<Player> pl = g->players[playerNum];
+    ifstream input(linkFile);
+    string word;
+    for (int id = 0; id < 8; id++) {
+        inputFile >> word;
+        char type = word[0];
+        int strength = word[1] - '0';
+        pl->setLink(type, strength, id);
+    }
+}
+
+void linkSetupRandom(unique_ptr<Game>& g, int playerNum) {
+    unique_ptr<Player> pl = g->players[playerNum];
+    vector<string> linkVals = {"D1", "D2", "D3", "D4", "V1", "V2", "V3", "V4"};
+    random_device rd;
+    mt19937 gen(rd());
+
+    shuffle(linkVals.begin(), linkVals.end(), gen);
+
+    for (int id = 0; id < 8; id++) {
+        char type = linkVals[id][0];
+        int strength = linkVals[id][1] - '0';
+        pl->setLink(type, strength, id);
+    }
+}
+
+void gridSetup(unique_ptr<Game>& g) {
     int row = 1;
     for (int col = 1; col <= 8; col++) {
         if (col != 4 && col != 5) {
-            getCell(row, col)->link = p1->link[col];
+            g->getCell(row, col)->link = g->p1->link[col];
         }
     }
     for (int col = 4; col <= 5; col++) {
-        getCell(row, col)->build->buildServer(p1);
+        g->getCell(row, col)->build->buildServer(p1);
     }
 
     row = 2;
     for (int col = 4; col <= 5; col++) {
-        getCell(row, col)->link = p1->link[col];
+        g->getCell(row, col)->link = g->p1->link[col];
     }
 
     row = 7;
     for (int col = 4; col <= 5; col++) {
-        getCell(row, col)->link = p2->link[col];
+        g->getCell(row, col)->link = g->p2->link[col];
     }
 
     row = 8;
     for (int col = 1; col <= 8; col++) {
         if (col != 4 && col != 5) {
-            getCell(row, col)->link = p2->link[col];
+            g->getCell(row, col)->link = g->p2->link[col];
         }
     }
     for (int col = 4; col <= 5; col++) {
-        getCell(row, col)->build->buildServer(p2);
+        g->getCell(row, col)->build->buildServer(p2);
     }
 }
 
-int main() {
-    unique_ptr<Grid> grid{new Grid};
-    unique_ptr<Player> p1 {new Player{1, "P1"}};
-    unique_ptr<Player> p2 {new Player{2, "P2"}};
+int main(int argc, char* argv[]) {
+    unique_ptr<Game> g{new Game};
 
-    PlayerSetup(p1, 'a');
-    PlayerSetup(p2, 'b');
-    GridSetup(grid, p1, p2);
-    p1->makeAllVisible();
-    
-    bool p1won = false;
-    bool p2won = false;
-    bool p1turn = true;
-    while (!p1won && !p2won) {
-        //let players take turns performing actions, as long as game not over
-        //prompt command input
-        //if move, check if link exists, and move for them if so
-        //if ability, check if ability exists, and use ability for them
+    const string DEFAULTAB = "LFDSP";
+
+    string abilities1;
+    string abilities2;
+    string linkFile1;
+    string linkFile2;
+
+    bool graphics = false;
+    bool ab1setup = false;
+    bool ab2setup = false;
+    bool links1setup = false;
+    bool links2setup = false;
+
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
+        if (arg == "-ability1" && i + 1 < argc) {
+            abilities1 = argv[++i];
+            abilitySetup(g, 0, abilities1);
+            ab1setup = true;
+        } else if (arg == "-ability2" && i + 1 < argc) {
+            abilities2 = argv[++i];
+            abilitySetup(g, 1, abilities2);
+            ab2setup = true;
+        } else if (arg == "-link1" && i + 1 < argc) {
+            linkFile1 = argv[++i];
+            linkSetup(g, 0, linkFile1);
+            links1setup = true;
+        } else if (arg == "-link2" && i + 1 < argc) {
+            linkFile2 = argv[++i];
+            linkSetup(g, 1, linkFile2);
+            links2setup = true;
+        } else if (arg == "-graphics") {
+            graphics = true;
+        }
     }
+    
+    if (!ab1setup) {
+        abilitySetup(g, 0, DEFAULTAB);
+    }
+
+    if (!ab2setup) {
+        abilitySetup(g, 1, DEFAULTAB);
+    }
+
+    if (!links1Setup) {
+        linkSetupRandom(g, 0);
+    }
+
+    if (!links1Setup) {
+        linkSetupRandom(g, 1);
+    }
+
+    gridSetup(g);
+    g->runGame();
 
     return 0;
 }
