142a8d85f2bd403133db6cce9ddec6633d5e07b5
Richard <87499486+RichardZhu02me@users.noreply.github.com>
2024-11-29 16:08:16 -0500

    merged from main

--- subject.cc
+++ subject.cc
@@ -2,24 +2,6 @@
 #include <vector>
 #include <algorithm>
 
-<<<<<<< HEAD
-void Subject::attach( Observer* o ) {
-    observers.push_back(std::unique_ptr<Observer>(o));
-}
-
-void Subject::detach( Observer* o ) {
-    for (auto it = observers.begin(); it != observers.end();) {
-        if (it->get() == o) {
-            it = observers.erase(it);
-        } else {
-            ++it;
-        }
-    }
-}
-
-void Subject::notifyObservers() {
-    for (auto& o : observers) {
-=======
 using namespace std;
 
 void Subject::attach(std::unique_ptr<Observer> o) {
@@ -36,17 +18,8 @@ void Subject::detach(Observer* o) {
 
 void Subject::notifyObservers() {
     for (const auto& o : observers) { // Use const reference for unique_ptr
->>>>>>> b741cdebbc603f2418c88221ad2b293115d086ce
         o->notify();
     }
 }
 
-<<<<<<< HEAD
-Subject::~Subject() {
-    for (auto it = observers.begin(); it != observers.end();) {
-        it = observers.erase(it);
-    }
-}
-=======
 Subject::~Subject() {}
->>>>>>> b741cdebbc603f2418c88221ad2b293115d086ce
